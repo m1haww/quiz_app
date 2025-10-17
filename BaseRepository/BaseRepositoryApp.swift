fb4fe003b814a561b5928bcde8cd4c709034e435
shulmnn <76616234+shulmnn@users.noreply.github.com>
2025-10-17 12:52:14 +0300

    Initial Commit

---
+++ BaseRepository/BaseRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseRepositoryApp.swift
+//  BaseRepository
+//
+//  Created by Matthew on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BaseRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
